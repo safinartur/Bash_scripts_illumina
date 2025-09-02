ffd9b327c06fb70542ba2bdd663bee35f62e1579
safinartur <122093892+safinartur@users.noreply.github.com>
2025-09-03 02:28:10 +0300

    Create ggsashimi_test.sh

---
+++ ggsashimi_test.sh
@@ -0,0 +1,8 @@
+ggsashimi \
+  -b ./tsv_test_sashimi.tsv \
+  -c chr19:1269332-1274810 \
+  -g /mnt/raid/artur/For_Dmitry/STAR_mapping/data/gencode.v46.annotation.gtf \
+  --shrink \
+  -M 10 \
+  -o ./sashimi_cirbp_ucsc.pdf
+
